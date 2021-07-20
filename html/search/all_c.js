3f78e6f8c8d0c2cc2cf475dfc567d54c7552e403
xiaoyi <xiaoyi@xd.com>
2021-07-20 19:25:40 +0800

    feat:update api doc

--- html/search/all_c.js
+++ html/search/all_c.js
@@ -1,8 +1,8 @@
 var searchData=
 [
   ['message_155',['message',['../class_tap_tap_1_1_moment_1_1_moment_callback_bean.html#a15d55953335ce2a7945aa4f40797e574',1,'TapTap.Moment.MomentCallbackBean.message()'],['../class_tap_tap_1_1_common_1_1_result.html#a9d22416784bc6c7f48d6a341d6b1b34a',1,'TapTap.Common.Result.message()'],['../class_tap_tap_1_1_common_1_1_tap_exception.html#a6edbe01553e53a74b51953494b886f4c',1,'TapTap.Common.TapException.message()']]],
-  ['method_156',['method',['../class_tap_tap_1_1_common_1_1_command.html#a774faf153bc2ce0791593f17ef58ff4d',1,'TapTap::Common::Command']]],
-  ['method_157',['Method',['../class_tap_tap_1_1_common_1_1_command_1_1_builder.html#a2466ea2a87a10cc1e3b7f38f58e7f9d2',1,'TapTap::Common::Command::Builder']]],
+  ['method_156',['Method',['../class_tap_tap_1_1_common_1_1_command_1_1_builder.html#a2466ea2a87a10cc1e3b7f38f58e7f9d2',1,'TapTap::Common::Command::Builder']]],
+  ['method_157',['method',['../class_tap_tap_1_1_common_1_1_command.html#a774faf153bc2ce0791593f17ef58ff4d',1,'TapTap::Common::Command']]],
   ['mobile_158',['Mobile',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#aa173e57450b330c256ee7a7dff5ea2d7',1,'TapTap::Bootstrap::TDSUser']]],
   ['mobileverified_159',['MobileVerified',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a063f415a077abbff6f47ff2c6fb6ce32',1,'TapTap::Bootstrap::TDSUser']]],
   ['momentcallbackbean_160',['MomentCallbackBean',['../class_tap_tap_1_1_moment_1_1_moment_callback_bean.html#a0bd2116028afc9078d21b0d768772c30',1,'TapTap.Moment.MomentCallbackBean.MomentCallbackBean()'],['../class_tap_tap_1_1_moment_1_1_moment_callback_bean.html',1,'TapTap.Moment.MomentCallbackBean']]],
--- html/search/functions_8.js
+++ html/search/functions_8.js
@@ -1,12 +1,12 @@
 var searchData=
 [
   ['login_552',['Login',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a61df21234d8077c847af044ed3c24e2d',1,'TapTap::Bootstrap::TDSUser']]],
-  ['loginanonymously_553',['LoginAnonymously',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a5381d7865113b3f26e362eb62d84cd20',1,'TapTap::Bootstrap::TDSUser']]],
+  ['loginanonymously_553',['LoginAnonymously',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a197c34cb42cbcbfe25bc0f637a43a3ad',1,'TapTap::Bootstrap::TDSUser']]],
   ['loginbyemail_554',['LoginByEmail',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a5118574e3d347718a8a180eca3723e5c',1,'TapTap::Bootstrap::TDSUser']]],
   ['loginbymobilephonenumber_555',['LoginByMobilePhoneNumber',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#ae7c68622c82e69ce290018cdb05c3a0f',1,'TapTap::Bootstrap::TDSUser']]],
   ['loginbysmscode_556',['LoginBySMSCode',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#aa83f738da07c5a37480a5d37f99c10a7',1,'TapTap::Bootstrap::TDSUser']]],
   ['loginwithauthdata_557',['LoginWithAuthData',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a97b6cac684d32ff2401144ad9a6521c3',1,'TapTap::Bootstrap::TDSUser']]],
-  ['loginwithauthdataandunionid_558',['LoginWithAuthDataAndUnionId',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a6ad0ebe6ccbd1dafd252395364b6c421',1,'TapTap::Bootstrap::TDSUser']]],
+  ['loginwithauthdataandunionid_558',['LoginWithAuthDataAndUnionId',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#aa9ac9050745207d7d90c1db39421babb',1,'TapTap::Bootstrap::TDSUser']]],
   ['loginwithtaptap_559',['LoginWithTapTap',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a916036e63d5965f747e0191feb6baf09',1,'TapTap::Bootstrap::TDSUser']]],
   ['logout_560',['Logout',['../class_tap_tap_1_1_bootstrap_1_1_t_d_s_user.html#a0142c277085a5759fe94f814ff94b4b7',1,'TapTap::Bootstrap::TDSUser']]]
 ];
